1e7a426b56bb19cc8eb006f886a976c3f63fc1d4
Joe Wesch <joe.wesch@networktocode.com>
2022-05-19 16:02:33 -0500

    Changed calculations to hours; fixed denominator

--- get_tempo.py
+++ get_tempo.py
@@ -1,8 +1,9 @@
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from dateutil.relativedelta import MO, relativedelta
 import os
 
 import requests
+import numpy
 
 
 WELLNESS_CODES = ["NI2-5", "NI2-6", "NI2-22", "NI2-23"]
@@ -47,38 +48,44 @@ def get_entries(start, end):
         response = requests.get(url, headers=headers)
 
 
-def get_context(start_date):
-    year_start = date(year=start_date.year, month=1, day=1)
-    quarter_start = date(year=start_date.year, month=((2 + start_date.month) // 3) * 3 - 2, day=1)
-    month_start = date(year=start_date.year, month=start_date.month, day=1)
-    week_start = start_date + relativedelta(weekday=MO(-1))
+def get_context(end_date):
+    year_start = date(year=end_date.year, month=1, day=1)
+    quarter_start = date(year=end_date.year, month=((2 + end_date.month) // 3) * 3 - 2, day=1)
+    month_start = date(year=end_date.year, month=end_date.month, day=1)
+    week_start = end_date + relativedelta(weekday=MO(-1))
+    # numpy.busday_count is not inclusive for the end date, so this adds an additional day
+    end_plus_one = end_date + timedelta(days=1)
     context = {
-        "total_year_minutes": 0,
+        "year_denominator": numpy.busday_count(year_start, end_plus_one) * 8,
+        "quarter_denominator": numpy.busday_count(quarter_start, end_plus_one) * 8,
+        "month_denominator": numpy.busday_count(month_start, end_plus_one) * 8,
+        "week_denominator": numpy.busday_count(week_start, end_plus_one) * 8,
+        "total_year_hours": 0,
         "total_year_billable": 0,
         "total_year_wellness": 0,
-        "total_quarter_minutes": 0,
+        "total_quarter_hours": 0,
         "total_quarter_billable": 0,
         "total_quarter_wellness": 0,
-        "total_month_minutes": 0,
+        "total_month_hours": 0,
         "total_month_billable": 0,
         "total_month_wellness": 0,
-        "total_week_minutes": 0,
+        "total_week_hours": 0,
         "total_week_billable": 0,
         "total_week_wellness": 0,
         "weeks": {},
     }
 
-    entries = get_entries(year_start.strftime("%Y-%m-%d"), start_date.strftime("%Y-%m-%d"))
+    entries = get_entries(year_start.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d"))
     for entry in entries:
-        entry_minutes = int(entry["timeSpentSeconds"] / 60)
-        entry_billable = int(entry["billableSeconds"] / 60)
+        entry_hours = float(entry["timeSpentSeconds"] / 3600)
+        entry_billable = float(entry["billableSeconds"] / 3600)
         if entry["issue"]["key"] in NON_BILLABLE_CODES:
-            entry_billable = 0
+            entry_billable = 0.0
         context["total_year_billable"] += entry_billable
         if entry["issue"]["key"] not in WELLNESS_CODES:
-            context["total_year_minutes"] += entry_minutes
+            context["total_year_hours"] += entry_hours
         else:
-            context["total_year_wellness"] += entry_minutes
+            context["total_year_wellness"] += entry_hours
 
         # if "QBR" in entry["description"] and "NI2-16" in entry["description"]:
         #     print(f"Possible QBR spotted: ({entry['startDate']}) {entry['description']}")
@@ -94,31 +101,31 @@ def get_context(start_date):
                 "well": 0,
             },
         )
-        context["weeks"][week_num]["total"] += entry_minutes
+        context["weeks"][week_num]["total"] += entry_hours
         if entry_billable:
             context["weeks"][week_num]["billable"] += entry_billable
         elif entry["issue"]["key"] in WELLNESS_CODES:
-            context["weeks"][week_num]["well"] += entry_minutes
+            context["weeks"][week_num]["well"] += entry_hours
         else:
-            context["weeks"][week_num]["nonbillable"] += entry_minutes
+            context["weeks"][week_num]["nonbillable"] += entry_hours
 
         if entry_date >= quarter_start:
             context["total_quarter_billable"] += entry_billable
             if entry["issue"]["key"] not in WELLNESS_CODES:
-                context["total_quarter_minutes"] += entry_minutes
+                context["total_quarter_hours"] += entry_hours
             else:
-                context["total_quarter_wellness"] += entry_minutes
+                context["total_quarter_wellness"] += entry_hours
         if entry_date >= month_start:
             context["total_month_billable"] += entry_billable
             if entry["issue"]["key"] not in WELLNESS_CODES:
-                context["total_month_minutes"] += entry_minutes
+                context["total_month_hours"] += entry_hours
             else:
-                context["total_month_wellness"] += entry_minutes
+                context["total_month_wellness"] += entry_hours
         if entry_date >= week_start:
             context["total_week_billable"] += entry_billable
             if entry["issue"]["key"] not in WELLNESS_CODES:
-                context["total_week_minutes"] += entry_minutes
+                context["total_week_hours"] += entry_hours
             else:
-                context["total_week_wellness"] += entry_minutes
+                context["total_week_wellness"] += entry_hours
 
     return context
--- tempo_cli.py
+++ tempo_cli.py
@@ -1,5 +1,5 @@
 import sys
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 
 from prettytable import PrettyTable
 
@@ -7,30 +7,36 @@ from get_tempo import get_context
 
 
 def main(args):
-    today = date.today()
+    # Default start date is yesterday
+    end_date = date.today() - timedelta(days=1)
     if args:
         try:
-            today = datetime.strptime(args[0], "%Y-%m-%d").date()
+            end_date = datetime.strptime(args[0], "%Y-%m-%d").date()
         except Exception as err:
             print(f"Unable to parse date: {err}")
-    context = get_context(today)
+    context = get_context(end_date)
+    print(f"End Date: {end_date}")
     print(
         "== Week ==\n"
-        f"Total Minutes: {context['total_week_minutes']} ({context['total_week_wellness']} Wellness)\n"
-        f"Billable Minutes: {context['total_week_billable']}\n"
-        f"Billable Percentage: {(context['total_week_billable'] / context['total_week_minutes'] * 100) if context['total_week_minutes'] else 0:0.2f}%\n\n"
+        f"Total Hours Expected: {context['week_denominator']}\n"
+        f"Total Hours Recorded: {context['total_week_hours']} ({context['total_week_wellness']} Wellness)\n"
+        f"Billable Hours: {context['total_week_billable']}\n"
+        f"Billable Percentage: {(context['total_week_billable'] / (context['week_denominator'] - context['total_week_wellness']) * 100) if context['total_week_hours'] else 0:0.2f}%\n\n"
         "== Month ==\n"
-        f"Total Minutes: {context['total_month_minutes']} ({context['total_month_wellness']} Wellness)\n"
-        f"Billable Minutes: {context['total_month_billable']}\n"
-        f"Billable Percentage: {(context['total_month_billable'] / context['total_month_minutes'] * 100) if context['total_month_minutes'] else 0:0.2f}%\n\n"
+        f"Total Hours Expected: {context['month_denominator']}\n"
+        f"Total Hours Recorded: {context['total_month_hours']} ({context['total_month_wellness']} Wellness)\n"
+        f"Billable Hours: {context['total_month_billable']}\n"
+        f"Billable Percentage: {(context['total_month_billable'] / (context['month_denominator'] - context['total_month_wellness']) * 100) if context['total_month_hours'] else 0:0.2f}%\n\n"
         "== Quarter ==\n"
-        f"Total Minutes: {context['total_quarter_minutes']} ({context['total_quarter_wellness']} Wellness)\n"
-        f"Billable Minutes: {context['total_quarter_billable']}\n"
-        f"Billable Percentage: {(context['total_quarter_billable'] / context['total_quarter_minutes'] * 100) if context['total_quarter_minutes'] else 0:0.2f}%\n\n"
+        f"Total Hours Expected: {context['quarter_denominator']}\n"
+        f"Total Hours Recorded: {context['total_quarter_hours']} ({context['total_quarter_wellness']} Wellness)\n"
+        f"Billable Hours: {context['total_quarter_billable']}\n"
+        f"Billable Percentage: {(context['total_quarter_billable'] / (context['quarter_denominator'] - context['total_quarter_wellness']) * 100) if context['total_quarter_hours'] else 0:0.2f}%\n\n"
         "== Year ==\n"
-        f"Total Minutes: {context['total_year_minutes']} ({context['total_year_wellness']} Wellness)\n"
-        f"Billable Minutes: {context['total_year_billable']}\n"
-        f"Billable Percentage: {(context['total_year_billable'] / context['total_year_minutes'] * 100) if context['total_year_minutes'] else 0:0.2f}%\n"
+        f"Total Hours Expected: {context['year_denominator']}\n"
+        f"Total Hours Recorded: {context['total_year_hours']} ({context['total_year_wellness']} Wellness)\n"
+        f"Billable Hours: {context['total_year_billable']}\n"
+        f"Billable Percentage: {(context['total_year_billable'] / (context['year_denominator'] - context['total_year_wellness']) * 100) if context['total_year_hours'] else 0:0.2f}%\n"
     )
     table = PrettyTable()
     table.field_names = ["Week Start", "Total", "Billable", "Non-Billable", "Wellness"]
